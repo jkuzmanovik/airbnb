8a484e3be13fea3f6988aafd4a17c2d5c0b8579a
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2023-06-09 12:50:20 +0200

    favorite functionality

---
+++ app/components/HearthButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { SafeUser } from "../types";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useFavorite from "../hooks/useFavorite";
+interface HearthButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const HearthButton: React.FC<HearthButtonProps> = ({
+  listingId,
+  currentUser,
+}) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
+
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="relative cursor-pointer transition hover:opacity-80"
+    >
+      <AiOutlineHeart
+        size={28}
+        className="absolute -right-[2px] -top-[2px] fill-white"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
+};
+
+export default HearthButton;
